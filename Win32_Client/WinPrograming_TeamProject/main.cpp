5517107929951b612cfc1ed30ca83e4e6513f006
HongTak <qoqo110966@gmail.com>
2022-06-08 04:34:44 +0900

    First Frame Work

---
+++ Win32_Client/WinPrograming_TeamProject/main.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+#include <tchar.h>
+
+HINSTANCE g_hinst;
+LPCTSTR lpszClass = L"Team Project ";
+LPCTSTR lpszWindowName = L"Chaihuantai";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hinst = hInstance;
+
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	WndClass.lpszMenuName = nullptr;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+	RegisterClassEx(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 1280, 800, nullptr, (HMENU)nullptr, hInstance, nullptr);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Message, 0, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return Message.wParam;
+}
+
+LRESULT __stdcall WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hDC;
+
+	// 메시지 처리하기
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		break;
+
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	// 이외의 메세지는 OS로
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
